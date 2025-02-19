0b90ce6e396e34d7ba0b46c66baaab1babc47b99
Carlos Juarez <cjuarez@uca.edu.sv>
2025-02-19 17:02:20 -0600

    actualización de ejercicio de serie de fibonnaci

--- Ejerciciio0219_SerieFibonacci.py
+++ Ejerciciio0219_SerieFibonacci.py
@@ -5,9 +5,11 @@ Created on Wed Feb 19 16:51:41 2025
 
 @author: carlosjuarez
 """
+def fibonacci(n):
+    fibo = [0, 1]
+    for i in range(n-2):
+        fibo.append(fibo[-1] + fibo[-2])#Agrega el último elemento de la lista y el penúltimo
+    return fibo 
 
 n = int(input('Ingrese cantidad de términos serie de Fibonacci: '))
-fibo = [0, 1]
-for i in range(n-2):
-    fibo.append(fibo[-1] + fibo[-2])
-print(fibo)
+print(fibonacci(n)[-1])
