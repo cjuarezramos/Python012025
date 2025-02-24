813255638158a095fca35f2ec8817601a9bbf711
Carlos Juarez <cjuarezramos@gmail.com>
2025-02-24 15:44:26 -0600

    Otro ejemplo de importacion

---
+++ Ejemplo0224_importacion03.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 15:23:47 2025
+
+@author: carlosjuarez
+Importar funciones forma 2
+"""
+
+from util import crear_datos_persona# importación de una funcion específica de util
+
+persona7 = crear_datos_persona('Juan', 'Perez', 20, 8.5)
+persona8 = crear_datos_persona('Maria', 'Flores', 18, 3)
+persona9 = crear_datos_persona('Fernanda', 'Lopez', 21, 10)
