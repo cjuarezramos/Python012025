96073b3641e4896f5b880e7870a2b0c9cb94a188
Carlos Juarez <cjuarezramos@gmail.com>
2025-02-24 15:45:36 -0600

    Ejemplos de clase

--- Ejemplo0224_importacion03.py
+++ Ejemplo0224_importacion03.py
@@ -9,6 +9,6 @@ Importar funciones forma 2
 
 from util import crear_datos_persona# importación de una funcion específica de util
 
-persona7 = crear_datos_persona('Juan', 'Perez', 20, 8.5)
-persona8 = crear_datos_persona('Maria', 'Flores', 18, 3)
-persona9 = crear_datos_persona('Fernanda', 'Lopez', 21, 10)
+persona4 = crear_datos_persona('Juan', 'Perez', 20, 8.5)
+persona5 = crear_datos_persona('Maria', 'Flores', 18, 3)
+persona6 = crear_datos_persona('Fernanda', 'Lopez', 21, 10)
